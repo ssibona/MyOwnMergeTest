f28d03cf10b5bc3fde91a2d1beeb976f14220990
ssibona <samuele.sibona@sysdig.com>
2017-08-08 13:50:36 -0700

    page added

---
+++ index.js
@@ -0,0 +1,17 @@
+function figo() {
+  this.route('login');
+  this.route('index', {path: '/'});
+  this.route('customers', function () {
+      this.route('new');
+  });
+  this.route('customer', {path: 'customers/:customer_id', resetNamespace: true}, function () {
+    this.route('agents');
+    this.route('users', function () {
+        this.route('new');
+    });
+    this.route('settings');
+    this.route('subscriptions');
+    this.route('fastpath-settings');
+    this.route('secure-settings');
+  });
+}
